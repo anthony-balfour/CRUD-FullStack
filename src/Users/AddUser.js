010654141c95b5b07d33a98b8638bfeffc8d59df
antb4 <ant5692@gmail.com>
2023-09-26 15:43:45 -0700

    submit and cancel form functionality

--- src/Users/AddUser.js
+++ src/Users/AddUser.js
@@ -1,6 +1,10 @@
+import axios from 'axios';
 import React, { useState } from 'react'
+import {Link, useNavigate } from 'react-router-dom'
 
 export default function AddUser() {
+
+  let navigate = useNavigate();
 
   // storing user info
 
@@ -15,60 +19,75 @@ export default function AddUser() {
   const onInputChange=(event) => {
     setUser({...user, [event.target.name]:event.target.value})
   }
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    // using axios to post the data to the database
+    await axios.post("http://localhost:8080/user", user);
+
+    // navigate to home page
+    navigate("/");
+  }
+
+  // for sending data to database and displaying inside of the home page table
   return (
     <aside className="container">
       <section className="row">
         <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
           <h2 className="text-center m-4">Register User</h2>
-          <section className="mb-3">
-            <label htmlFor="Name" className="form-label">
-              Name
-            </label>
-            <input
-            type={"text"}
-            className="form-control"
-            placeholder='Enter name'
-            name="name"
-            value = {name}
-            onChange = {(event) => onInputChange(event)}
-            />
-          </section>
+          <form onSubmit={(event) => {
+            onSubmit(event);
+          }}>
+            <section className="mb-3">
+              <label htmlFor="Name" className="form-label">
+                Name
+              </label>
+              <input
+              type={"text"}
+              className="form-control"
+              placeholder='Enter name'
+              name="name"
+              value = {name}
+              onChange = {(event) => onInputChange(event)}
+              />
+            </section>
 
-          <section className="mb-3">
-            <label htmlFor="Username" className="form-label">
-              Username
-            </label>
-            <input
-            type={"text"}
-            className="form-control"
-            placeholder='Enter username'
-            name="username"
-            value = {username}
-            onChange = {(event) => onInputChange(event)}
-            />
-          </section>
+            <section className="mb-3">
+              <label htmlFor="Username" className="form-label">
+                Username
+              </label>
+              <input
+              type={"text"}
+              className="form-control"
+              placeholder='Enter username'
+              name="username"
+              value = {username}
+              onChange = {(event) => onInputChange(event)}
+              />
+            </section>
 
-          <section className="mb-3">
-            <label htmlFor="Email" className="form-label">
-              Email
-            </label>
-            <input
-            type={"text"}
-            className="form-control"
-            placeholder='Enter email'
-            name="email"
-            value = {email}
-            onChange = {(event) => onInputChange(event)}
-            />
-          </section>
+            <section className="mb-3">
+              <label htmlFor="Email" className="form-label">
+                Email
+              </label>
+              <input
+              type={"text"}
+              className="form-control"
+              placeholder='Enter email'
+              name="email"
+              value = {email}
+              onChange = {(event) => onInputChange(event)}
+              />
+            </section>
 
-        <button type="submit" className="btn btn-outline-primary">
-          Submit
-        </button>
+          <button type="submit" className="btn btn-outline-primary">
+            Submit
+          </button>
 
-        <button type="submit" className="mx-2 btn btn-outline-danger">
-          Cancel
-        </button>
+          <Link to="/" className="mx-2 btn btn-outline-danger">
+            Cancel
+          </Link>
+        </form>
         </div>
       </section>
     </aside>
