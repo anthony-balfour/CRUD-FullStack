cf596bd96d62ecb7a55072ae6c001a2f87e04073
antb4 <ant5692@gmail.com>
2023-09-25 18:41:27 -0700

    added Bootstrap styling to navbar

--- src/Layout/Navbar/navbar.js
+++ src/Layout/Navbar/navbar.js
@@ -4,9 +4,9 @@ export default function Navbar() {
   return (
     <div>
 
-<nav className="navbar navbar-expand-lg bg-body-tertiary">
+<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <a className="navbar-brand" href="#">Full Stack CRUD Application</a>
     <button
     className="navbar-toggler"
     type="button"
@@ -16,6 +16,7 @@ export default function Navbar() {
     aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
+    <button className="btn btn-outline-light">Add User</button>
   </div>
 </nav>
 
